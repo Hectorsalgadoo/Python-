94a211e01c69ff627dcab3f914cba50b1a2ed0c8
Hectorsalgadoo <hectorsoria2@outlook.com>
2025-03-21 01:32:43 -0300

    aula de quinta if e else

---
+++ py/aula-20-03.py
@@ -0,0 +1,59 @@
+x=0
+y="0"
+
+if x==y:
+    x=x+y
+    print("entrou no if")
+else:
+    print("entrou no else")
+
+
+
+
+
+
+#x=true 
+#print(type(x))
+
+#x = input("digite um numero: ")
+#print(x,type(x))
+#x=int(x)
+#print(x,type(x))
+#print(x%2==0)
+
+#x=int(input("digite um numero: "))
+
+#FAZENDO A CONTA DE IMPAR PRIMEIRO 
+#if x%2==0:
+#    if x==0:
+#        print("x e zero ")
+#    else:
+#        print(f"{x} e par")
+#else:
+#    print(f"{x} e impar")
+#print("termonou")
+
+
+ #FAZENDO A CONTA DE PAR PRIMEIRO   
+#if x==0:
+#    print("x e zero")
+#elif x==0:
+#    print(f"{x} e par")
+#else:
+#    print(f"{x} e impar")
+#print("terminou")
+
+
+#y=""
+#print(y)
+#print(type(y))
+#print(len(y))
+#print(int(y))
+
+
+#if x == 0 :
+#    print ("entrou if")
+#    print(x==0)
+#else:
+#    print("entrou else")
+#print("terminou")
